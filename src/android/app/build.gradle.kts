0d70d27fa6208d3960570da1265f75ffe9983379
hellbound2307 <marwannaili.23.07@gmail.com>
2026-09-08 04:05:01 +0100

    Version 1.18-x2 (vc37): payload persistence + global mounts + netguard hardening release

--- src/android/app/build.gradle.kts
+++ src/android/app/build.gradle.kts
@@ -39,8 +39,8 @@ android {
         applicationId = "com.openminis.app.x"
         minSdk = 26
         targetSdk = 35
-        versionCode = 36
-        versionName = "1.18-x1"
+        versionCode = 37
+        versionName = "1.18-x2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
