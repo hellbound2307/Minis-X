158c1b78ff477da41c8cd2ab95b9b4523017f5a1
hellbound2307 <hellbound2307@users.noreply.github.com>
2026-09-13 03:11:49 +0100

    vc43 / 1.19-x6: py-meta-tools release version bump

--- src/android/app/build.gradle.kts
+++ src/android/app/build.gradle.kts
@@ -39,8 +39,8 @@ android {
         applicationId = "com.openminis.app.x"
         minSdk = 26
         targetSdk = 35
-        versionCode = 42
-        versionName = "1.19-x5"
+        versionCode = 43
+        versionName = "1.19-x6"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
